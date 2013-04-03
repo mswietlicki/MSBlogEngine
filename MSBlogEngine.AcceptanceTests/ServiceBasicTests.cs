1c1b2adf1f815bab061a5f6bf6b0f87b8fb33ca3
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-03 17:49:11 +0200

    added first test method

--- MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
+++ MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -9,5 +11,15 @@ namespace MSBlogEngine.AcceptanceTests
 {
     public class ServiceBasicTests
     {
+        [Fact]
+        public void IsServiceAccesible()
+        {
+            using (var web = new HttpClient())
+            {
+                var response = web.GetAsync("").Result;
+
+                Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
+            }
+        }
     }
 }
