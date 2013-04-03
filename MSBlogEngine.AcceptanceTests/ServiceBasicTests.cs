f333f1c8816d90019961f56eda8d0ea8d7dbe1b5
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-03 20:57:19 +0200

    Adding SelfHost server

--- MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
+++ MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
 using Xunit;
 
 namespace MSBlogEngine.AcceptanceTests
@@ -14,12 +15,21 @@ namespace MSBlogEngine.AcceptanceTests
         [Fact]
         public void IsServiceAccesible()
         {
-            using (var web = new HttpClient() { BaseAddress = new Uri("http://localhost:8023")})
+            var baseAddress = new Uri("http://localhost:8023");
+
+            var configuration = new HttpSelfHostConfiguration(baseAddress);
+            var server = new HttpSelfHostServer(configuration);
+            
+            server.OpenAsync();
+
+            using (var web = new HttpClient() { BaseAddress = baseAddress })
             {
                 var response = web.GetAsync("").Result;
 
                 Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
             }
+
+            server.CloseAsync();
         }
     }
 }
