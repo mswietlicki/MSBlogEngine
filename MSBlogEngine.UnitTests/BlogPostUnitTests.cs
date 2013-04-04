d0213d1e06b6f9b88ca4e449d67016539c0ff767
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-04 17:04:16 +0200

    Added unit tests to test BlogPost custom equality

---
+++ MSBlogEngine.UnitTests/BlogPostUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSBlogEngine.Models;
+using Xunit;
+
+namespace MSBlogEngine.UnitTests
+{
+    public class BlogPostUnitTests
+    {
+        [Fact]
+        public void TwoSamePostsEqual()
+        {
+            var a = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+            var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+
+            Assert.Equal(a, b);
+        }       
+        
+        [Fact]
+        public void TwoNotSamePostsNotEqual()
+        {
+            {
+                var a = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+                var b = new BlogPost { Title = "Tes", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+
+                Assert.NotEqual(a, b);
+            }
+            {
+                var a = new BlogPost { Title = "Test", Body = "tet2", CreateDate = new DateTime(2011, 1, 1) };
+                var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+
+                Assert.NotEqual(a, b);
+            }
+            {
+                var a = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1, 4, 23, 11) };
+                var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+
+                Assert.NotEqual(a, b);
+            }
+        }        
+
+    }
+}
