40f39e2815a01a9d3d0b8e7f2aa88e482dcab2d1
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-04 15:56:27 +0200

    Implemented simple blog post storage for one post

--- MSBlogEngine/Controllers/BlogController.cs
+++ MSBlogEngine/Controllers/BlogController.cs
@@ -6,18 +6,22 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using MSBlogEngine.Models;
 
 namespace MSBlogEngine.Controllers
 {
     public class BlogController : ApiController
     {
+        private static BlogPost _blogPost;
+
         public HttpResponseMessage Get()
         {
-            return Request.CreateResponse(HttpStatusCode.OK, "");
+            return Request.CreateResponse(HttpStatusCode.OK, _blogPost);
         }
 
-        public HttpResponseMessage Put()
+        public HttpResponseMessage Put(BlogPost post)
         {
+            _blogPost = post;
             return Request.CreateResponse(HttpStatusCode.OK, "");
         }
     }
