ac0678dd492ba4daea6351c3b7537e6ef6942991
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-04 15:50:30 +0200

    Extend put test to get sended value

--- MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
+++ MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 using MSBlogEngine.Models;
+using Newtonsoft.Json;
 using Xunit;
 using MSBlogEngine;
 
@@ -43,17 +44,28 @@ namespace MSBlogEngine.AcceptanceTests
 
             using (var web = new HttpClient(server) { BaseAddress = baseAddress })
             {
-                var response = web.PutAsync("",
-                    new BlogPost
-                        {
-                            Title = "Test",
-                            CreateDate = new DateTime(2013, 4, 4, 15, 30, 30),
-                            Body = "Test body"
-                        }, new JsonMediaTypeFormatter()).Result;
+                var blogPost = new BlogPost
+                    {
+                        Title = "Test",
+                        CreateDate = new DateTime(2013, 4, 4, 15, 30, 30),
+                        Body = "Test body"
+                    };
+
+                var response = web.PutAsJsonAsync("", blogPost).Result;
 
                 Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
+
+                response = web.GetAsync("").Result;
+
+                Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
+                var result = response.Content.ReadAsStringAsync().Result;
+                var expected = JsonConvert.SerializeObject(blogPost);
+
+                Assert.True(result.Equals(expected), "Result equal: " + result);
             }
         }
+
+
     }
 
 }
