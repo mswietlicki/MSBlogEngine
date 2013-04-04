530a4aef1595c2d595955f93f8288de7672b422e
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-04 17:18:20 +0200

    Refactoring: Use TheoryAtribute to test multiple cases

--- MSBlogEngine.UnitTests/BlogPostUnitTests.cs
+++ MSBlogEngine.UnitTests/BlogPostUnitTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MSBlogEngine.Models;
 using Xunit;
+using Xunit.Extensions;
 
 namespace MSBlogEngine.UnitTests
 {
@@ -17,30 +18,28 @@ namespace MSBlogEngine.UnitTests
             var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
 
             Assert.Equal(a, b);
-        }       
-        
-        [Fact]
-        public void TwoNotSamePostsNotEqual()
+        }
+
+
+        public static IEnumerable<object[]> ExampleNotSamePostData
         {
+            get
             {
-                var a = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
-                var b = new BlogPost { Title = "Tes", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
-
-                Assert.NotEqual(a, b);
+                return new[]
+                    {
+                        new object[] { new BlogPost {Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1)}, new BlogPost {Title = "Tes", Body = "tet", CreateDate = new DateTime(2011, 1, 1) }},
+                        new object[] { new BlogPost {Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1)}, new BlogPost {Title = "Test", Body = "tet2", CreateDate = new DateTime(2011, 1, 1) }},
+                        new object[] { new BlogPost {Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1)}, new BlogPost {Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1, 4,23,11) }}
+                    };
             }
-            {
-                var a = new BlogPost { Title = "Test", Body = "tet2", CreateDate = new DateTime(2011, 1, 1) };
-                var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
+        }
 
-                Assert.NotEqual(a, b);
-            }
-            {
-                var a = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1, 4, 23, 11) };
-                var b = new BlogPost { Title = "Test", Body = "tet", CreateDate = new DateTime(2011, 1, 1) };
-
-                Assert.NotEqual(a, b);
-            }
-        }        
+        [Theory]
+        [PropertyData("ExampleNotSamePostData")]
+        public void TwoNotSamePostsNotEqual(BlogPost a, BlogPost b)
+        {
+            Assert.NotEqual(a, b);
+        }
 
     }
 }
