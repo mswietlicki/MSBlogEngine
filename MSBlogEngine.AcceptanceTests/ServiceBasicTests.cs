c196b184688ff81b72bebbeaf3e0a2a78a3b3365
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-04 15:31:37 +0200

    Added PutBlogPost test and BlogPost model

--- MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
+++ MSBlogEngine.AcceptanceTests/ServiceBasicTests.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
+using MSBlogEngine.Models;
 using Xunit;
 using MSBlogEngine;
 
@@ -22,7 +24,7 @@ namespace MSBlogEngine.AcceptanceTests
             var configuration = new HttpSelfHostConfiguration(baseAddress);
             new Bootstrapper().Configure(configuration);
             var server = new HttpSelfHostServer(configuration);
-            
+
             using (var web = new HttpClient(server) { BaseAddress = baseAddress })
             {
                 var response = web.GetAsync("").Result;
@@ -30,6 +32,28 @@ namespace MSBlogEngine.AcceptanceTests
                 Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
             }
         }
+        [Fact]
+        public void PutBlogPostToService()
+        {
+            var baseAddress = new Uri("http://localhost:8023");
+
+            var configuration = new HttpSelfHostConfiguration(baseAddress);
+            new Bootstrapper().Configure(configuration);
+            var server = new HttpSelfHostServer(configuration);
+
+            using (var web = new HttpClient(server) { BaseAddress = baseAddress })
+            {
+                var response = web.PutAsync("",
+                    new BlogPost
+                        {
+                            Title = "Test",
+                            CreateDate = new DateTime(2013, 4, 4, 15, 30, 30),
+                            Body = "Test body"
+                        }, new JsonMediaTypeFormatter()).Result;
+
+                Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
+            }
+        }
     }
-    
+
 }
---
+++ MSBlogEngine/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MSBlogEngine.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Body { get; set; }
+    }
+}
