ee87aa5cb48f56d9d5e58a63f0548fe5d5e1af45
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-05 17:16:39 +0200

    Implemented XmlBlogStorage.GetPost

--- MSBlogEngine/XMLBlogStorage.cs
+++ MSBlogEngine/XMLBlogStorage.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 using MSBlogEngine.Models;
 
 namespace MSBlogEngine
 {
     public class XMLBlogStorage: IBlogStorage
     {
+        private readonly IFileStorage _fileStorage;
+
+        public XMLBlogStorage()
+        {
+            _fileStorage = Global.Container.GetInstance<IFileStorage>();
+        }
+
         public BlogPost GetPost(int id)
         {
-            throw new NotImplementedException();
+            using (var stream = _fileStorage.GetFileStream(string.Format("Posts\\{0}.xml", id)))
+            {
+                return new XmlSerializer(typeof(BlogPost)).Deserialize(stream) as BlogPost;
+            }
         }
+    }
+
+    public interface IFileStorage
+    {
+        Stream GetFileStream(string format);
     }
 }
