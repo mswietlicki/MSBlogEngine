f295d2001693b40e3e9c563069f097c7f0e2219e
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-09 15:24:28 +0200

    Refactoring - moved files around

--- MSBlogEngine/Storage/XMLBlogStorage.cs
+++ MSBlogEngine/Storage/XMLBlogStorage.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Serialization;
 using MSBlogEngine.Models;
-using MSBlogEngine.Storage;
 using MSBlogEngine.Storage.FileStorage;
 
-namespace MSBlogEngine
+namespace MSBlogEngine.Storage
 {
     public class XMLBlogStorage: IBlogStorage
     {
