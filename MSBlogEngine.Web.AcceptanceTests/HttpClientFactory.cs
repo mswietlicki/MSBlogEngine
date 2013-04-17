695adc1eb0ce6e8d3a258244964eaf26c699b0f9
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-17 17:46:10 +0200

    Added MSBlogEngine.Web.AcceptanceTests and MSBlogEngine.Web

---
+++ MSBlogEngine.Web.AcceptanceTests/HttpClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using SimpleInjector;
+
+namespace MSBlogEngine.Web.AcceptanceTests
+{
+    public class HttpClientFactory
+    {
+        public HttpClient Create()
+        {
+            var baseAddress = new Uri("http://localhost:8023");
+
+            var configuration = new HttpSelfHostConfiguration(baseAddress);
+            Global.Container.GetInstance<Bootstrapper>().Configure(configuration, Global.Container);
+            var server = new HttpSelfHostServer(configuration);
+
+            return new HttpClient(server) {BaseAddress = baseAddress};
+        }
+    }
+}
---
+++ MSBlogEngine.Web.AcceptanceTests/WebpageTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSBlogEngine.Web.AcceptanceTests;
+using Xunit;
+
+namespace MSBlogEngine.AcceptanceTests
+{
+    public class WebpageTests
+    {
+        [Fact]
+        public void IsWebpageAccesible()
+        {
+            using (var web = new HttpClientFactory().Create())
+            {
+                var response = web.GetAsync("").Result;
+
+                Assert.True(response.IsSuccessStatusCode, "Status code : " + response.StatusCode);
+            }
+
+        }
+    }
+}
