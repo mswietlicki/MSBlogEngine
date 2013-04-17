3a32f7eaab2ef07c543158dc8b1323d69c553e29
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-17 18:42:21 +0200

    Added IISExpressHttpHandler

--- MSBlogEngine.Web.AcceptanceTests/HttpClientFactory.cs
+++ MSBlogEngine.Web.AcceptanceTests/HttpClientFactory.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.SelfHost;
+using IISExpressAutomation;
 using SimpleInjector;
 
 namespace MSBlogEngine.Web.AcceptanceTests
@@ -15,7 +18,26 @@ namespace MSBlogEngine.Web.AcceptanceTests
         {
             var baseAddress = new Uri("http://localhost:8022");
 
-            return new HttpClient() { BaseAddress = baseAddress };
+            var path = Path.GetFullPath("..\\..\\..\\MSBlogEngine.Web");
+            var port = baseAddress.Port;
+
+            return new HttpClient(new IISExpressHttpHandler(path, port)) { BaseAddress = baseAddress };
+        }
+
+    }
+    public class IISExpressHttpHandler : HttpClientHandler
+    {
+        private readonly IISExpress _iis;
+
+        public IISExpressHttpHandler(string path, int port)
+        {
+            _iis = new IISExpress(new Parameters { Path = path, Port = port });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _iis.Dispose();
         }
     }
 }
