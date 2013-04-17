5887850e5e90aa12180cfc976447ab77076f0357
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-04-17 18:50:08 +0200

    Added PostController and changed routemap

---
+++ MSBlogEngine.Web/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSBlogEngine.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new ContentResult() { Content = "Welcome" };
+        }
+    }
+}
