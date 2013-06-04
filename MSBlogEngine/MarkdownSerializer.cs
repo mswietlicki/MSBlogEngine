3480e628cc259d9d9bc48611a9c4fbbc9c5c9de6
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-06-04 13:54:16 +0200

    Refactoring

--- MSBlogEngine/MarkdownSerializer.cs
+++ MSBlogEngine/MarkdownSerializer.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using MSBlogEngine.Models;
 using MarkdownSharp;
 
 namespace MSBlogEngine
