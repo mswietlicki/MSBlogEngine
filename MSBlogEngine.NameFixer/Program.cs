faa7458ee629ca9234aadb80d88fa7c48ac1916d
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-07-13 16:19:28 +0200

    Added NameFixer project

---
+++ MSBlogEngine.NameFixer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSBlogEngine.NameFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any())
+            {
+                Console.Error.WriteLine("Directory needed!");
+            }
+
+            var dir = new DirectoryInfo(args[0]);
+
+            foreach (var file in dir.GetFiles())
+            {
+                var name = file.Name;
+
+                name = name.ToLower();
+
+                name = RemoveAccent(name);
+                name = name.Replace(" ", "-");
+                file.MoveTo(dir.FullName + "\\" + name);
+            }
+        }
+
+        public static string RemoveAccent(string txt)
+        {
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+
+            return Encoding.ASCII.GetString(bytes);
+
+        }
+    }
+}
