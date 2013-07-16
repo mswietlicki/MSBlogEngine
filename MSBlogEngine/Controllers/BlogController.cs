54870495f7a1a4fa9ade880aeefe2ff253d36b4f
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-07-16 17:23:19 +0200

    Move BlogsController

--- MSBlogEngine/Controllers/BlogController.cs
+++ MSBlogEngine/Controllers/BlogController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +17,6 @@ namespace MSBlogEngine.Controllers
             _blogProvider = blogProvider;
         }
 
-        //public List<BlogPost> Get()
-        //{
-        //    return _blogProvider.GetPosts().Where(post => !post.Hidden).OrderByDescending(post => post.CreateDate).ToList();
-        //}
-
         public BlogPost Get(string id)
         {
             return _blogProvider.GetPost(id);
@@ -38,35 +30,6 @@ namespace MSBlogEngine.Controllers
         public void Post(string id, BlogPost post)
         {
             _blogProvider.UpdatePost(id, post);
-        }
-    }
-
-    public class BlogsController : ApiController
-    {
-        private readonly IBlogProvider _blogProvider;
-
-        public BlogsController(IBlogProvider blogProvider)
-        {
-            _blogProvider = blogProvider;
-        }
-
-        public IEnumerable<BlogPost> Get()
-        {
-            return GetPosts().ToList();
-        }
-
-        public IEnumerable<BlogPost> Get(string tag)
-        {
-            return GetPosts()
-                        .Where(p =>
-                            string.IsNullOrWhiteSpace(tag) ||
-                            p.Tags.Any(t => t.Equals(tag, StringComparison.InvariantCultureIgnoreCase)))
-                        .ToList();
-        }
-
-        private IEnumerable<BlogPost> GetPosts()
-        {
-            return _blogProvider.GetPosts().Where(post => !post.Hidden).OrderByDescending(post => post.CreateDate);
         }
     }
 }
---
+++ MSBlogEngine/Controllers/BlogsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using MSBlogEngine.Models;
+using MSBlogEngine.Providers;
+
+namespace MSBlogEngine.Controllers
+{
+    public class BlogsController : ApiController
+    {
+        private readonly IBlogProvider _blogProvider;
+
+        public BlogsController(IBlogProvider blogProvider)
+        {
+            _blogProvider = blogProvider;
+        }
+
+        public IEnumerable<BlogPost> Get()
+        {
+            return GetPosts().ToList();
+        }
+
+        public IEnumerable<BlogPost> Get(string tag)
+        {
+            return GetPosts()
+                .Where(p =>
+                       string.IsNullOrWhiteSpace(tag) ||
+                       p.Tags.Any(t => t.Equals(tag, StringComparison.InvariantCultureIgnoreCase)))
+                .ToList();
+        }
+
+        private IEnumerable<BlogPost> GetPosts()
+        {
+            return _blogProvider.GetPosts().Where(post => !post.Hidden).OrderByDescending(post => post.CreateDate);
+        }
+    }
+}
