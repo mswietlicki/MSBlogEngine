b09e75c5bcbb87e8a83686af3e38595c7dbdce60
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-07-18 13:27:59 +0200

    Remove all facebook references and move layout to center.

--- MSBlogEngine.Web/Scripts/facebookInit.js
+++
@@ -1,7 +0,0 @@
-﻿(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/pl_PL/all.js#xfbml=1&appId=405859169532623";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
